4dc02e46b493d7bca7d3475b0bb55295ab426dac
Mattquach93 <133842648+Mattquach93@users.noreply.github.com>
2023-07-10 15:30:17 -0500

    commit

---
+++ MathFunctions.c
@@ -0,0 +1,11 @@
+# include <studio.h>
+#include "MathFunctions.h"
+int add (int ( first number))
+{
+    return firstNumber + secondNumber;
+}
+
+int subtract( int firstNumber, int secondNumber)
+{
+    return firstNumber - secondNumber;
+}
---
+++ Tutorial Project C/MathFunctions.h
@@ -0,0 +1,3 @@
+int add(int fristNumber, int secondNumber);
+int subtract(int firstNumber, int secondNumber);
+int multiply(int firstNumber, int secondNumber);
